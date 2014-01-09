b922d4a9ff0f22dbb727a402bb8a4b3461ec79c3
Dr Owain Kenway <o.kenway@ucl.ac.uk>
2014-01-09 15:24:27 +0000

    Started Day2

--- HPCandHTCusingLegion/gentex.sh
+++ HPCandHTCusingLegion/gentex.sh
@@ -5,6 +5,8 @@
 #
 # Owain Kenway
 # 7/January/2014
+#
+# 8/January/2014 - added day 2
 
 # Get the template from our rsdpandoc directory
 cp -R /usr/lib/python2.7/site-packages/rsdpandoc/assets .
@@ -15,12 +17,19 @@ cp -R src/assets/* assets
 # Pandoc it
 pandoc --template=assets/report -V documentclass=scrartcl -V links-as-notes --filter pandoc-citeproc --default-image-extension=png -V linkcolor=uclmidgreen --number-sections src/day1.md -o day1.tex
 
+pandoc --template=assets/report -V documentclass=scrartcl -V links-as-notes --filter pandoc-citeproc --default-image-extension=png -V linkcolor=uclmidgreen --number-sections src/day1.md -o day2.tex
+
 # PDFlatex it (twice to deal with labels).
 pdflatex day1.tex
 pdflatex day1.tex
+pdflatex day2.tex
+pdflatex day2.tex
+
 mkdir -p slides/day1
+mkdir -p slides/day2
 mv day1.pdf slides/day1/slides.pdf
+mv day2.pdf slides/day2
 
 # Tidy up
-rm day1.*
+rm day1.* day2.*
 rm -rf assets
